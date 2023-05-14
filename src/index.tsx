a19503072675025b35a3f7c050c227a0ac4c2e3e
woongsnote <woongsnote@gmail.com>
2023-05-14 16:29:23 +0900

    refactor: root 디렉토리 ts 적용

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import App from './App';
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
