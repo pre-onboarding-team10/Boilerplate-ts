d1cce4b1c8b90b39ae6d92f22813b6badc74b8e7
jeongminsang <zjzj@hanmail.net>
2023-05-14 23:14:42 +0900

    "feat: TS 적용하기"

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+
+  return (
+    <header className="header_style">
+      <h1 className="title_style">Toodos</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/hooks/useFocus.tsx
@@ -0,0 +1,17 @@
+import { useRef } from "react";
+
+interface UseFocusProps {
+  ref: any;
+  setFocus: () => void;
+}
+
+const useFocus = (): UseFocusProps => {
+  const ref = useRef<HTMLElement>(null);
+  const setFocus = () => {
+    ref.current && ref.current.focus();
+  };
+
+  return { ref, setFocus };
+};
+
+export default useFocus;
