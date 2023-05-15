78ef2818d7d39389c5d3e91c075c20d388e9ba49
hyukzz <ri_chard@naver.com>
2023-05-16 02:37:21 +0900

    feat: rendering dropdown

--- src/components/todo/InputTodo.tsx
+++ src/components/todo/InputTodo.tsx
@@ -5,6 +5,9 @@ import useFocus from '../../hooks/useFocus';
 import { SetStateType, TodoDataType } from '../../types/types';
 import ItemButton from './ItemButton';
 import { handleCreateTodos } from '../../utils/todos';
+import { suggestionData } from '../../api/todo';
+import useDebounce from '../../utils/debounce';
+import SuggestionsDropdown from '../dropDown/SuggestionsDropdown';
 
 type InputTodoProps = {
   setTodos: SetStateType<TodoDataType[]>;
@@ -13,7 +16,10 @@ type InputTodoProps = {
 const InputTodo = ({ setTodos }: InputTodoProps) => {
   const [inputText, setInputText] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [suggestions, setSuggestions] = useState<string[]>([]);
   const { ref, setFocus } = useFocus();
+
+  const debouncedInputText = useDebounce(inputText.toLowerCase(), 500);
 
   useEffect(setFocus, [setFocus]);
 
@@ -28,6 +34,22 @@ const InputTodo = ({ setTodos }: InputTodoProps) => {
     [inputText, setTodos]
   );
 
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      try {
+        const response = await suggestionData(debouncedInputText);
+        const result = (response as { result: string[] }).result;
+        setSuggestions(result);
+      } catch (error) {
+        console.error('Error fetching suggestions:', error);
+      }
+    };
+
+    if (debouncedInputText) {
+      fetchSuggestions();
+    }
+  }, [debouncedInputText]);
+
   return (
     <form className="form-container" onSubmit={handleSubmitForm}>
       <input
@@ -38,6 +60,17 @@ const InputTodo = ({ setTodos }: InputTodoProps) => {
         onChange={e => setInputText(e.target.value)}
         disabled={isLoading}
       />
+
+      {suggestions.length > 0 && (
+        <SuggestionsDropdown
+          suggestions={suggestions}
+          setSuggestions={setSuggestions}
+          debouncedInputText={debouncedInputText}
+          setTodos={setTodos}
+          setInputText={setInputText}
+        />
+      )}
+
       {!isLoading ? (
         <ItemButton mode="add" />
       ) : (
