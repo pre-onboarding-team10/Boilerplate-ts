f825aafe20e55d4c35d73acee0e887bec32982e3
blueline1984 <blueline1318@naver.com>
2023-05-15 15:03:40 +0900

    refact: useFocus 수정

--- src/hooks/useFocus.ts
+++ src/hooks/useFocus.ts
@@ -1,10 +1,14 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 const useFocus = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setFocus = () => {
     ref.current && ref.current.focus();
   };
+
+  useEffect(() => {
+    setFocus();
+  }, [setFocus]);
 
   return { ref, setFocus };
 };
