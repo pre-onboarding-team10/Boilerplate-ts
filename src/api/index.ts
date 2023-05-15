f7176803b8f71b239703b160a11c0180708bec8d
Ain Chung <artnouveau0804@gmail.com>
2023-05-15 17:59:49 +0900

    update: ApiRequest 타입 수정
    인빈님 코드 참고

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,10 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { TodoDataType, TodoInputType } from '../types/types';
+import axios, { AxiosPromise, AxiosRequestConfig } from 'axios';
 
-export type BaseInstance = {
-  url: string;
-  request?: AxiosRequestConfig;
-  data?: TodoInputType;
-  config?: AxiosRequestConfig;
-};
+interface ApiRequestType {
+  get: (url: string, request?: AxiosRequestConfig) => AxiosPromise;
+  delete: (url: string, request?: AxiosRequestConfig) => AxiosPromise;
+  post: <T>(url: string, data: T, config?: AxiosRequestConfig) => AxiosPromise;
+}
 
 const baseURL = process.env.REACT_APP_API_URL;
 const token = process.env.REACT_APP_TOKEN;
@@ -20,12 +18,10 @@ const baseInstance = axios.create({
 
 baseInstance.interceptors.response.use(({ data }) => data);
 
-const apiRequest = {
-  get: ({ url, request }: BaseInstance) =>
-    baseInstance.get<TodoDataType[]>(url, request),
-  delete: ({ url, request }: BaseInstance) => baseInstance.delete(url, request),
-  post: ({ url, data, config }: BaseInstance) =>
-    baseInstance.post(url, data, config),
+const apiRequest: ApiRequestType = {
+  get: (url, request) => baseInstance.get(url, request),
+  delete: (url, request) => baseInstance.delete(url, request),
+  post: (url, data, config) => baseInstance.post(url, data, config),
 };
 
 export default apiRequest;
