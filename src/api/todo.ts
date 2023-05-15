80ac047d8f58200c70747cd9f106b61b7969f29d
hyukzz <ri_chard@naver.com>
2023-05-16 02:09:43 +0900

    feat: todo&suggestion api

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,34 +1,53 @@
-import { TodoInputType } from '../types/types';
+import { TodoInputType, TodoDataType } from '../types/types';
 import apiRequest from './index';
 
 const RESOURCE = '/todos';
 
-export const getTodoList = async () => {
+export const getTodoList = async (): Promise<TodoDataType[]> => {
   try {
-    const response = await apiRequest.get({ url: `${RESOURCE}` });
+    const response = await apiRequest.get<TodoDataType[]>(`${RESOURCE}`);
 
-    return response;
+    return response.data;
   } catch (error) {
     throw new Error('API getTodoList error');
   }
 };
 
-export const createTodo = async (data: TodoInputType) => {
+export const createTodo = async (
+  data: TodoInputType
+): Promise<TodoDataType> => {
   try {
-    const response = await apiRequest.post({ url: `${RESOURCE}`, data });
+    const response = await apiRequest.post<TodoDataType, typeof data>(
+      `${RESOURCE}`,
+      data
+    );
 
-    return response;
+    return response.data;
   } catch (error) {
     throw new Error('API createTodo error');
   }
 };
 
-export const deleteTodo = async (id: string) => {
+export const deleteTodo = async (id: string): Promise<void> => {
   try {
-    const response = await apiRequest.delete({ url: `${RESOURCE}/${id}` });
-
-    return response;
+    await apiRequest.delete(`${RESOURCE}/${id}`);
   } catch (error) {
     throw new Error('API deleteTodo error');
+  }
+};
+
+export const suggestionData = async (debouncedInputText: string) => {
+  try {
+    const response = await apiRequest.get('/search', {
+      params: {
+        q: debouncedInputText,
+        page: 1,
+        limit: 10,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching data:');
   }
 };
