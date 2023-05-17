991a59ccd183e03c602574b8f0980dbd6aa6bbea
woongsnote <woongsnote@gmail.com>
2023-05-17 14:57:26 +0900

    test: 스피너 test id 추가 및 테스트 코드 구현

--- src/components/base/Spinner.tsx
+++ src/components/base/Spinner.tsx
@@ -1,7 +1,7 @@
 import { ImSpinner8 } from 'react-icons/im';
 
 const Spinner = () => {
-  return <ImSpinner8 className="spinner" />;
+  return <ImSpinner8 className="spinner" data-testid="spinner-icon" />;
 };
 
 export default Spinner;
---
+++ src/components/test/Spinner.test.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import Spinner from '../base/Spinner';
+import '@testing-library/jest-dom';
+
+describe('Spinner', () => {
+  it('renders without crashing', () => {
+    render(<Spinner />);
+  });
+
+  it('renders the ImSpinner8 icon', () => {
+    const { getByTestId } = render(<Spinner />);
+    expect(getByTestId('spinner-icon')).toBeInTheDocument();
+  });
+
+  it('has a "spinner" class', () => {
+    const { container } = render(<Spinner />);
+    expect(container.firstChild).toHaveClass('spinner');
+  });
+});
