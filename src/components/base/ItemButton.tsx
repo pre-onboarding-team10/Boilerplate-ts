2c1d03e7cbe1a5787e9a078ae5febf9e62b050f1
woongsnote <woongsnote@gmail.com>
2023-05-17 14:54:57 +0900

    fix: 아이콘 대신 children 으로 변경

--- src/components/base/ItemButton.tsx
+++ src/components/base/ItemButton.tsx
@@ -1,21 +1,21 @@
 import { ReactNode } from 'react';
 
 type Props = {
-  icon: ReactNode;
+  children: ReactNode;
   handleClick?: () => void;
   className?: string;
   type?: 'button' | 'submit';
 };
 
 const ItemButton = ({
-  icon,
+  children,
   handleClick,
   className,
   type = 'button',
 }: Props) => {
   return (
     <button className={className} type={type} onClick={handleClick}>
-      {icon}
+      {children}
     </button>
   );
 };
